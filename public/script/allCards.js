61b583a0114a7a6bda231e22291dd5736b0cd90f
Maya Lynn <mayalynncohen@gmail.com>
2023-01-29 18:56:05 -0800

    changed existing cards and added them to all cards

--- public/script/allCards.js
+++ public/script/allCards.js
@@ -1,10 +1,20 @@
 fetch("/api/tarotCards").then(res=>res.json()).then(data=>{
     console.log(data)
-    data.forEach(card=>{
+    for(let i = 0; i < data.length; i++){
+        const myDiv = document.createElement('div')
         const myLi = document.createElement("li");
         const image = document.createElement("img")
-        image.src = card.imgSrc
-        myLi.appendChild(image)
+        const title = document.createElement('h3')
+        title.textContent = data[i].cardName
+        image.src = data[i].imgSrc
+        myDiv.appendChild(image)
+        myDiv.appendChild(title)
+        myLi.appendChild(myDiv)
         document.getElementById('allCards').appendChild(myLi)
-    })
+        if(i%2==0){
+            myDiv.classList.add('even')
+        } else {
+            myDiv.classList.add('odd')
+        }
+    }
 })
