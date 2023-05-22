a9a0fd45b49b4c01308cb00319796affa0047b65
Maya Lynn <mayalynncohen@gmail.com>
2023-05-21 20:33:41 -0700

    added get reading by Id

--- controllers/readingController.js
+++ controllers/readingController.js
@@ -19,21 +19,61 @@ const jwt = require("jsonwebtoken");
 router.get("/", async (req, res) => {
     const token = req.headers?.authorization?.split(" ")[1];
     if (!token) {
-        return res.status(403).json({ msg: "you must be logged in to a Reading" });
+        return res.status(403).json({ msg: "you must be logged in to get readings" });
     }
     try {
         const tokenData = jwt.verify(token, process.env.JWT_SECRET);
-        
+
         const userReadings = await Reading.findAll({
             where: {
                 UserId: tokenData.id
             },
-            include: [{model:Position, include: [Card]}]
+            attributes: ["id", "readingDate", "eventDate", "question", "generalTake"],
+            include: [{
+                model: Position,
+                attributes: ["id"],
+                include: [{
+                    model: Card,
+                    attributes: ["id", "cardName", "cardNumber", "arcana", "suit"]
+                }]
+            }]
         })
         res.json(userReadings)
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Error getting the current users readings." });
+    }
+})
+
+// Get Reading by Id
+router.get("/:readingId", async (req, res) => {
+    const token = req.headers?.authorization?.split(" ")[1];
+    if (!token) {
+        return res.status(403).json({ msg: "you must be logged in to get this reading" });
+    }
+    try {
+        const tokenData = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userReadings = await Reading.findByPk(req.params.readingId, {
+            include: [{
+                model: Position,
+                attributes: ["id", "position", "personalInterpretation"],
+                include: [{
+                    model: Card,
+                    attributes: ["id", "cardName", "cardNumber", "arcana", "suit", "meaning", "reveresedMeaning"]
+                }]
+            }]
+        })
+        if(!userReadings){
+            return res.status(404).json({ msg: "No reading found with this Id" });
+        }
+        if(tokenData.id!=userReadings.UserId){
+            return res.status(403).json({ msg: "you must be logged in to get this reading" });
+        }
+        res.json(userReadings)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error getting this reading by Id." });
     }
 })
 
@@ -87,12 +127,12 @@ router.post("/", async (req, res) => {
             })
         })
 
-        res.json({msg: 'Reading successfully added!'})
+        res.json({ msg: 'Reading successfully added!' })
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Error adding your Reading." });
     }
-    
+
 })
 
 router.delete("/deleteReading/:readingId", async (req, res) => {
@@ -104,19 +144,19 @@ router.delete("/deleteReading/:readingId", async (req, res) => {
         const tokenData = jwt.verify(token, process.env.JWT_SECRET);
         const readingToDelete = await Reading.findByPk(req.params.readingId)
 
-        if(!readingToDelete){
-            return res.status(404).json({ msg: "Deleting Reading: Reading not found"})
+        if (!readingToDelete) {
+            return res.status(404).json({ msg: "Deleting Reading: Reading not found" })
         }
 
-        if(readingToDelete.UserId != tokenData.id) {
-            return res.status(403).json({ msg: "Deleting Reading: Reading does not belong to current user"})
+        if (readingToDelete.UserId != tokenData.id) {
+            return res.status(403).json({ msg: "Deleting Reading: Reading does not belong to current user" })
         }
 
         const deletedData = await readingToDelete.destroy()
 
-        return res.json({ msg: "Reading deleted"})
+        return res.json({ msg: "Reading deleted" })
 
-    }catch (err) {
+    } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Error deleting your Reading." });
     }
